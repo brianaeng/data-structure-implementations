86b411254906d0349273f19f6ec29ba7250c5530
Briana Eng <brianaeng@gmail.com>
2017-05-07 22:39:46 -0700

    Adding comments for things to implement

--- stack.go
+++ stack.go
@@ -1,3 +1,5 @@
 // Implement Top for a Stack using only Push(e) and Pop. (Note: This is an example of extending functionality without knowing the implementation details of how the Stack is implemented i.e. not knowing whether the Stack is implemented using an array or a linked list). Assume the data contained is int.
 // Write a function to reverse a string (the funciton takes a string as input parameter) using a Stack.
 // Write a function to print all integer values in a binary tree in pre-order traversal iteratively. (Hint: Use a Stack.)
+// Top() which returns the item that would be popped next  (for Stack)
+
