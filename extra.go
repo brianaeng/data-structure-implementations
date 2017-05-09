0944f88e4a33389f2cb74fcd2e961c9d2a9ba3cd
Briana Eng <brianaeng@gmail.com>
2017-05-09 08:05:23 -0700

    More problems using stacks/queues

---
+++ extra.go
@@ -0,0 +1,6 @@
+// Write a function to print all integer values in a binary tree in breadth first traversal iteratively. (Hint: Use a Queue.)
+// Write a function to print all integer values in a binary tree in pre-order traversal iteratively. (Hint: Use a Stack.)
+// How would you implement a Queue using 2 Stacks?
+// How would you implement a Stack using 2 Queues?
+
+package main
