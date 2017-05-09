30601e484590cddfe255f690ee018589cc0a6c5e
Briana Eng <brianaeng@gmail.com>
2017-05-09 08:04:53 -0700

    Starting stack implementation

--- stack.go
+++ stack.go
@@ -1,5 +1,29 @@
 // Implement Top for a Stack using only Push(e) and Pop. (Note: This is an example of extending functionality without knowing the implementation details of how the Stack is implemented i.e. not knowing whether the Stack is implemented using an array or a linked list). Assume the data contained is int.
-// Write a function to reverse a string (the funciton takes a string as input parameter) using a Stack.
-// Write a function to print all integer values in a binary tree in pre-order traversal iteratively. (Hint: Use a Stack.)
 // Top() which returns the item that would be popped next  (for Stack)
+
+package main 
+
+type Stack struct {
+	x []int
+	size int
+}
+
+func (stack *Stack) Top() int {
+	return stack.x[0]
+
+	//way of doing this with only pop & push:
+}
+
+func (stack *Stack) Empty() bool {
+	if stack.size == 0 {
+		return true
+	}
+
+	return false
+}
+
+// Write a function to reverse a string (the funciton takes a string as input parameter) using a Stack.
+func (stack *Stack) Reverse(str string) string {
+
+}
 
