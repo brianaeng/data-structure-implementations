a2c10197e542b247cc5d33c5b8fa471e72ca7e84
Briana Eng <brianaeng@gmail.com>
2017-05-14 19:25:01 -0700

    Pre/Post/Inorder traversals (recursive) added

--- trees.go
+++ trees.go
@@ -1,10 +1,29 @@
-// Delete a given value from a Binary Search Tree using recursive solution.
-// Print all values in a Binary Tree using Pre-order, Post-order and In-order traversals. Implement each solution recursively.
 // Find the height of Binary Tree. Implement the recursive solution.
 // Insert a given value in a Binary Search Tree. Implement recursive solution.
 // Find a given value in a Binary Search Tree. Implement recursive solution. 
 
+//also do all the problems iteratively
+
+//insert the nodes and then print them out in the right order
+
+//given a string, from string & to string (of same length) - first char of from string should be replaced by from
+//find & replace given to and from
+
+//operating system concepts - dinosaur book
+//dining philosopher
+
+//Thursday after class --trees depth first traversal (preorder, postorder, etc.)
+//convert stacks/queue to linked list
+
+//cs stanford
+
+//string manipulation using bit arrays
+
 package main
+
+import (
+	"fmt"
+)
 
 type Tree struct {
 	root *Node
@@ -16,6 +35,73 @@ type Node struct {
 	right *Node
 }
 
-func (tree *Tree) Delete(value int) *Tree {
+// Print all values in a Binary Tree using Pre-order traversal - root, left, right
+func (tree *Tree) Preorder(node *Node) {
+	if node == nil {
+		return 
+	}
+
+	fmt.Println(node.value)
+
+	tree.Preorder(node.left)
+
+	tree.Preorder(node.right)
+}
+
+// Print all values in a Binary Tree using Post-order traversal - left, right, root
+func (tree *Tree) Postorder(node *Node) {
+	if node == nil {
+		return
+	}
+
+	tree.Postorder(node.left)
+
+	tree.Postorder(node.right)
+
+	fmt.Println(node.value)
+}
+
+// Print all values in a Binary Tree using In-order traversal - left, root, right
+func (tree *Tree) Inorder(node *Node) {
+	if node == nil {
+		return
+	}
+
+	tree.Inorder(node.left)
+
+	fmt.Println(node.value)
+
+	tree.Inorder(node.right)
+}
+
+func (tree *Tree) DepthFirst(node *Node) {
+
+}
+
+func (tree *Tree) BreadthFirst(node *Node) {
+
+}
+
+// Delete a given value from a Binary Search Tree using recursive solution.
+func (tree *Tree) Delete(value int) bool {
+	currentNode := tree.root
+
+	return tree.RemoveNode(currentNode, value)
+}
+
+func (tree *Tree) RemoveNode(node *Node, value int) bool {
+	if node == nil {
+		return node
+	}
+
+	if value < node.value {
+		node.left = tree.RemoveNode(node.left, value)
+	} else if value > node.value {
+		node.right = tree.RemoveNode(node.right, value)
+	} else {
+		if node.left == nil {
+
+		}
+	}
 	
 }
