49e126c580c7b8720e47be9261c2aef9a5ac8f9c
Briana Eng <brianaeng@gmail.com>
2017-05-14 19:25:26 -0700

    Binary search added (iterative)

--- extra.go
+++ extra.go
@@ -5,6 +5,55 @@
 
 package main
 
+import (
+	"fmt"
+)
+
+//Search a sorted array for a value
+// func RecursiveBinarySearch(value int, arr []int) bool {
+// 	currentIndex := (len(arr) -1)/2 //
+// 	currentItem := arr[currentIndex]
+// 	subArray := arr
+
+// 	if currentItem == value {
+// 		//Return its place
+// 		return true
+// 	} else if currentItem > value {
+// 		subArray = subArray[:currentIndex]
+// 		fmt.Println("in here is num is bigger than value")
+// 		fmt.Println(subArray)
+// 		RecursiveBinarySearch(value, subArray)
+// 	} else if currentItem < value {
+// 		subArray = subArray[currentIndex:]
+// 		fmt.Println("in here is num is smaller than value")
+// 		fmt.Println(subArray)
+// 		RecursiveBinarySearch(value, subArray)
+// 	}
+
+// 	return false
+// }
+
+func IterativeBinarySearch(value int, arr []int) int {
+	low := 0
+	high := len(arr) - 1
+
+	for low <= high {
+		mid := (high + low) / 2
+
+		if arr[mid] == value {
+			return mid
+		} else if arr[mid] < value {
+			low = mid + 1
+		} else if arr[mid] > value {
+			high = mid - 1
+		}
+	}
+
+	return -1
+}
+
 func main() {
-	
+	startArray := []int{1,2,3,4,5,6}
+	result := IterativeBinarySearch(5, startArray)
+	fmt.Println(result)
 }
